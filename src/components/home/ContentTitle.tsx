8fa51483cd9f71c3fb6f2e70ae35a239ebd1e2ce
ByungHo <qudgh4290@gmail.com>
2022-03-16 23:59:33 +0900

    feat: 타이틀 추가

---
+++ src/components/home/ContentTitle.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+interface ContentTitleProps {
+  iconName: string;
+  title: string;
+  moreViewLink: string | null;
+}
+
+const ContentTitle: React.FC<ContentTitleProps> = ({iconName, title, moreViewLink}) => {
+  return (
+    <Main>
+      <div>
+        <img src={iconName} alt="인기 검색 국회의원" />
+        <h3>{title}</h3>
+      </div>
+      {moreViewLink
+        ? <DetailLink to={moreViewLink}>더보기</DetailLink>
+        : null
+      }
+    </Main>
+  );
+}
+
+const Main = styled.div`
+  margin: 0 0 8px;
+  padding: 0 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h2 {
+    font-size: 1.8rem;
+    color: #2B2B2B;
+  }
+  h3 {
+    font-size: 1.6rem;
+    color: #2B2B2B;
+  }
+  > div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const DetailLink = styled(Link)`
+  font-size: 1.2rem;
+  color: #818181;
+  &:hover {
+    color: #545454;
+  }
+`;
+
+export default ContentTitle;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import {HeaderLogo} from "../components/Header";
 import {Link} from "react-router-dom";
 import SearchInput from "../components/SearchInput";
+import SearchRedIcon from "../assets/icons/Search_red.svg";
+import ContentTitle from "../components/home/ContentTitle";
 
 const HomePage: React.FunctionComponent = () => {
   const [state, setState] = useState<string>("");
@@ -16,6 +18,26 @@ const HomePage: React.FunctionComponent = () => {
       <HeaderLogo />
       <Slider />
       <SearchInput />
+      <Container>
+        <ContentTitle
+          iconName={SearchRedIcon}
+          title="인기 검색 국회의원 TOP 4"
+          moreViewLink=""
+        />
+        <LawmakerGrid>
+          <li>
+            <div className="lawmaker-list__image">
+              <img src="" alt="사진" />
+            </div>
+            <p>정진석</p>
+          </li>
+        </LawmakerGrid>
+        <ContentTitle
+          iconName={SearchRedIcon}
+          title="오늘 국회 일정"
+          moreViewLink="test"
+        />
+      </Container>
     </div>
   );
 }
@@ -24,6 +46,41 @@ const Slider = styled.div`
   width: 100%;
   height: 180px;
   background-color: #A9A9A9;
+`;
+
+const Container = styled.div`
+  padding: 0 0 24px;
+  width: 100%;
+`;
+
+const LawmakerGrid = styled.ul`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  li {
+    margin: 8px 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #2B2B2B;
+    p {
+      font-weight: 500;
+      font-size: 1.6rem;
+    }
+    .lawmaker-list__image {
+      margin-bottom: 4px;
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+      border: 1px solid #d9d9d9;
+      overflow: hidden;
+      > img {
+        width: 64px;
+      }
+    }
+  }
+  @media screen and (max-width: 430px) { grid-template-columns: repeat(4, 1fr); }
+  @media screen and (max-width: 348px) { grid-template-columns: repeat(3, 1fr); }
 `;
 
 export default HomePage;
